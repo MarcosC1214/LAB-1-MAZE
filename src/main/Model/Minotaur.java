edba491b676f2345e9cb14612c213f19daeabf8b
Marcos Carrillo <83974878+0373067@users.noreply.github.com>
2021-05-12 19:56:31 -0400

    Update Minotaur.java

--- src/main/Model/Minotaur.java
+++ src/main/Model/Minotaur.java
@@ -1,13 +1,13 @@
 package main.Model;
 
-public class Minotaur {
+public class Minotaur { // Minotaur class containing all geetters and setters.
 
 	private int _row, _col;
 	private boolean _isAlive;
 	
-	public int getRow() { return _row; }
-	public int getCol() { return _col; }
-	public void setPos(int r, int c) { _row = r; _col = c; }
+	public int getRow() { return _row; } // Getter for the rows.
+	public int getCol() { return _col; } // Getter for the colums.
+	public void setPos(int r, int c) { _row = r; _col = c; } // Setter for the position.
 	
 	public Minotaur(int r, int c) {
 		_row = r;
@@ -15,6 +15,6 @@ public class Minotaur {
 		_isAlive = true;
 	}
 	
-	public boolean isAlive() { return _isAlive; }
+	public boolean isAlive() { return _isAlive; } // Checker to check if is alive.
 }
 
