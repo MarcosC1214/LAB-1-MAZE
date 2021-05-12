201ff341c6d92980a41712686f51ee608bbd3996
Marcos Carrillo <83974878+0373067@users.noreply.github.com>
2021-05-12 17:07:54 -0400

    Update JOP.java

--- src/main/Controller/JOP.java
+++ src/main/Controller/JOP.java
@@ -1,14 +1,14 @@
-package main.Controller;
+package main.Controller; // JOP Class in charge of all JOP.in and JOP.msg pop ups.
 
 import javax.swing.JOptionPane;
 
 public class JOP {
 
-	public static void msg(String msg) {
+	public static void msg(String msg) { // Method in charge of messages in pop ups.
 		JOptionPane.showMessageDialog(null, msg);
 	}
 	
-	public static String in(String msg){
+	public static String in(String msg) { // Method in charge of message along with String input value. 
 		return JOptionPane.showInputDialog(msg);
 	}
 }
