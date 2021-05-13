0e320395f982116a96e687542f1ec8777873ee51
Marcos Carrillo <83974878+0373067@users.noreply.github.com>
2021-05-12 20:02:37 -0400

    Update Sword.java

--- src/main/Model/Sword.java
+++ src/main/Model/Sword.java
@@ -1,11 +1,11 @@
 package main.Model;
 
-public class Sword {
+public class Sword { // Sword class for player along with getters and setters.
 	
 	private int _row, _col;
 	
-	public int getRow() { return _row; }
-	public int getCol() { return _col; }
+	public int getRow() { return _row; } // Getter fir rows.
+	public int getCol() { return _col; } // Getter for colums.
 	
 	public Sword(int r, int c) {
 		_row = r;
