112817bd3ecf18e2b69c8aabb88d7df2acf4140b
Marcos Carrillo <83974878+0373067@users.noreply.github.com>
2021-05-12 20:01:31 -0400

    Update Spider.java

--- src/main/Model/Spider.java
+++ src/main/Model/Spider.java
@@ -1,12 +1,12 @@
 package main.Model;
 
-public class Spider {
+public class Spider { // Class for 2nd monster consistin of spiders and getters and setters.
 	private int _row, _col;
 	boolean _spiDead;
 	
-	public int getRow() { return _row; }
-	public int getCol() { return _col; }
-	public void setPos(int r, int c) { _row = r; _col = c; }
+	public int getRow() { return _row; } // Getter for rows.
+	public int getCol() { return _col; } // Getter for colums.
+	public void setPos(int r, int c) { _row = r; _col = c; } // Setter for spider position.
 	
 	public Spider(int r, int c) {
 		_row = r;
@@ -14,5 +14,5 @@ public class Spider {
 		_spiDead = false;
 	}
 	
-	public boolean isAlive() { return _spiDead; }
+	public boolean isAlive() { return _spiDead; } // Boolean checker if spider is alive.
 }
